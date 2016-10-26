9e645cf78f4307500482a3ba7a59d62e0996ebab
Josh Johnson <josh@jshjohnson.com>
2016-10-26 14:01:44 +0100

    Setup webpack 2 build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,55 +3,59 @@ var package = require('./package.json');
 var webpack = require('webpack');
 var wrapperPlugin = require('wrapper-webpack-plugin');
 var banner = `/*! ${ package.name } v${ package.version } | (c) ${ new Date().getFullYear() } ${ package.author } | ${ package.homepage } */ \n`;
-var minimize = process.argv.indexOf('--minimize') !== -1;
 
-var config = {
-  devtool: 'cheap-module-source-map',
-  entry: [
-    './assets/scripts/src/choices'
-  ],
-  output: {
-    path: path.join(__dirname, '/assets/scripts/dist'),
-    filename: minimize ? 'choices.min.js' : 'choices.js',
-    publicPath: '/assets/scripts/dist/',
-    library: 'Choices',
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  libraryTarget: 'umd',
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        // This has effect on the react lib size
-        'NODE_ENV': JSON.stringify('production'),
+module.exports = function(env) {
+  var minimize = env && env.minimize ? true : false;
+  var config = {
+      devtool: 'cheap-module-source-map',
+      entry: [
+        './assets/scripts/src/choices'
+      ],
+      output: {
+        path: path.join(__dirname, '/assets/scripts/dist'),
+        filename: minimize ? 'choices.min.js' : 'choices.js',
+        publicPath: '/assets/scripts/dist/',
+        library: 'Choices',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+      },
+      plugins: [
+        new webpack.DefinePlugin({
+          'process.env': {
+            // This has effect on the react lib size
+            'NODE_ENV': JSON.stringify('production'),
+          }
+        }),
+        new wrapperPlugin({
+          header: banner,
+        }),
+      ],
+      module: {
+        loaders: [{
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: ['babel'],
+          include: path.join(__dirname, 'assets/scripts/src'),
+        }]
       }
-    }),
-    new wrapperPlugin({
-      header: banner,
-    }),
-  ],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'assets/scripts/src')
-    }]
-  }
-};
-
-if (minimize) {
-  config.plugins.unshift(new webpack.optimize.UglifyJsPlugin({
-    sourceMap: false,
-    mangle: true,
-    output: {
-      comments: false
-    },
-    compress: {
-      warnings: false,
-      screw_ie8: true
     }
-  }));
-}
 
-module.exports = config;
+  if (minimize) {
+    config.plugins.unshift(new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      mangle: true,
+      output: {
+        comments: false
+      },
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        dead_code: true,
+        unused: true
+      }
+    }));
+  }
+
+  return config;
+
+};
